e49a43d17784fcf43b84707cd464d505f2151db8
Konrad Kühne <konnykuehne@gmail.com>
2016-11-29 14:54:18 +0100

    use web mode for css files

--- core/modes/kordano-web.el
+++ core/modes/kordano-web.el
@@ -72,6 +72,7 @@
   (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
